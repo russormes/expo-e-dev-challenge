5fcd18994777f42261019ded62fba1ad9ae86a08
Russell Ormes <russormes@gmail.com>
2017-06-10 23:08:22 +0100

    Use jQuery to populate the dropdowns and table.
    Mock out the calls to the model. Next add the server calls.

--- client/index.js
+++ client/index.js
@@ -1,3 +1,62 @@
 const model = require("./model");
 
-model.getProductsBySupplier( 34 );
+function initView(){
+	model.getSuppliers( function( err, supData ){
+		if( err ){
+			//Report the probelm (Obviously not in an ALERT box")
+			return alert( err.message );
+		}
+
+		const selSupplier = $('#selSupplier');
+
+		selSupplier.find('option').remove();
+		$.each( supData, function( key, value ) {
+			$('<option>').val( key ).text( value ).appendTo( selSupplier );
+		});
+
+		return selSupplier.change();
+
+	} )
+}
+
+$("#selSupplier").change(
+	function( e ){
+		const supId = e.target.options[e.target.selectedIndex].value;
+		model.getProductsBySupplier( supId, function( err, prodJson ){
+
+			const selProduct = $('#selProduct');
+
+			selProduct.find('option').remove();
+			$.each( prodJson, function(key, value) {
+				$('<option>').val( key ).text( value.product ).appendTo( selProduct );
+			});
+
+			selProduct.change();
+
+		});
+
+	}
+);
+
+$("#selProduct").change(
+
+	function( e ){
+
+		const prodId = e.target.options[e.target.selectedIndex].value;
+		model.getProductById( prodId, function( err, prodJson ){
+
+			$('#tabDetails tr').not(':first').remove();
+			let html = '';
+			html += '<tr><td>' + prodId +
+				'</td><td>' + prodJson.supplier + '</td>' +
+				'</td><td>' + prodJson.product + '</td>' +
+				'</td><td>' + prodJson.price + '</td></tr>';
+			$('#tabDetails tr').first().after(html);
+
+		});
+
+	}
+
+);
+
+initView();
--- client/model.js
+++ client/model.js
@@ -2,6 +2,47 @@
 
 const MODEL = module.exports = {};
 
-MODEL.getProductsBySupplier = function( supId ){
-	console.log( "Supplier Id is ", supId );
+let productsInfo = {};
+
+MODEL.getSuppliers = function( next ){
+	return next( null,
+		{
+			"1": "New Co ltd",
+			"2": "Old Co ltd"
+		}
+	);
+};
+
+MODEL.getProductsBySupplier = function( supId, next ){
+	//Test
+	if( supId === "1" ){
+		productsInfo = {
+			"1" : { "product": "Small wongle", "price": "5", "supplier": "New Co ltd" },
+			"2" : { "product": "Large wongle", "price": "8", "supplier": "New Co ltd" },
+			"3" : { "product": "Super wongle", "price": "12", "supplier": "New Co ltd" }
+		};
+	} else {
+		productsInfo = {
+			"4" : { "product": "Mini wongle", "price": "4", "supplier": "New Co ltd" },
+			"5" : { "product": "Small wongle", "price": "6", "supplier": "New Co ltd" },
+			"6" : { "product": "Large wongle", "price": "9", "supplier": "New Co ltd" },
+			"7" : { "product": "Super wongle", "price": "13", "supplier": "New Co ltd" }
+		};;
+	}
+	next( null, productsInfo );
 }
+
+MODEL.getProductById = function( prodId, next ){
+	let prodData;
+	if( productsInfo ){
+		prodData = productsInfo[ prodId ];
+		if( prodData ){
+			return next( null, prodData );
+		} else {
+			return next( new Error("No product with id " + prodId ) )
+		}
+	} else {
+		//Go get the data?
+		next( new Error("Something strange happened! No product data present") );
+	}
+};
